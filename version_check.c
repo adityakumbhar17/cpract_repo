957855acb421d84122bc4775b854f8b6975f0a51
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-03 15:32:41 +0530

    this is v3

--- version_check.c
+++ version_check.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 int main() {    
     int a, b, c; 
-    float average; 
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c); 
-    average = (a + b + c) / 3.0; 
-    printf("Average = %.2f\n", average); 
-    return 0; 
-    //this is version 2 
+    //print this number  add 10 in each numer and print that number
+    printf("Enter three numbers: ");    
+    scanf("%d %d %d", &a, &b, &c); //sanning the input numbers from the user
+    a=a+10; // Add 10 to a
+    b=b+10; // Add 10 to b
+    c=c+10; // Add 10 to c
+    printf("Numbers after adding 10: %d %d %d\n", a, b, c); // Output the numbers after adding 10
+    int average = (a + b + c) / 3; // Calculate the average
+    printf("Average = %d\n", average); // Output the average
+
+    //this is version 3
 }
