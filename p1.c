4cb3ca77f4ad6ffe4bcf9c10d64c9b21c3bbd881
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-03 14:57:11 +0530

    add new extra variable  for the calculating the differance

--- p1.c
+++ p1.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 int main() {
     int a, b, c; //variables to store the input numbers and the result 
+    int d; //variable to store the difference of a and b
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
     c=a+b; // Calculate the sum of a and b
+    d=a-b; // Calculate the difference of a and b;
     printf("Sum = %d\n", c); // Output the result hii hello 
+    printf("Difference = %d\n", d); // Output the difference
+printf("Hello World\n"); // Output Hello World
     return 0; 
 }
