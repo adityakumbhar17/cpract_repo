c36c4ba679877773c0126738bdae3834ca0d80d3
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-03 15:11:18 +0530

    fix: correct spelling in comment for input scanning

--- p1.c
+++ p1.c
@@ -4,7 +4,7 @@ int main() {
     int a, b, c; //variables to store the input numbers and the result 
     int d; //variable to store the difference of a and b
     printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &a, &b); //sanning the input numbers from the user
     c=a+b; // Calculate the sum of a and b
     d=a-b; // Calculate the difference of a and b;
     printf("Sum = %d\n", c); // Output the result hii hello 
