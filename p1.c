4abc3469b85bab5a0ab3dbed88f7ab72b14295a4
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-03 14:28:44 +0530

    add comment in the project for valedeting the  the git is working or not

---
+++ p1.c
@@ -0,0 +1,10 @@
+//write the program to add two numbers
+#include <stdio.h>
+int main() {
+    int a, b, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum = %d\n", sum); // Output the result hii hello 
+    return 0;
+}
