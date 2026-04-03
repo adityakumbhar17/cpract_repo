2e79d889f0ea04f9a2fc4a97ccb3b8027dced6e9
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-03 15:27:14 +0530

    this is v2

--- version_check.c
+++ version_check.c
@@ -1,12 +1,12 @@
 //write the code for fide the avrage of three numbers
 #include <stdio.h>
 int main() {    
-    int a, b, c; //variables to store the input numbers
-    float average; //variable to store the average of the three numbers
+    int a, b, c; 
+    float average; 
     printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c); //scanning the input numbers from the user
-    average = (a + b + c) / 3.0; // Calculate the average of a, b and c
-    printf("Average = %.2f\n", average); // Output the average with 2 decimal places
+    scanf("%d %d %d", &a, &b, &c); 
+    average = (a + b + c) / 3.0; 
+    printf("Average = %.2f\n", average); 
     return 0; 
-    //this is version 1 
+    //this is version 2 
 }
