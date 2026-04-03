b3f8e79bb6f8fd82f34763e6d95026db448b94a1
Aditya kumbhar <aditya.kumbhar@ashidaelectronics.in>
2026-04-03 14:48:33 +0530

    changed

--- p1.c
+++ p1.c
@@ -1,10 +1,10 @@
 //write the program to add two numbers
 #include <stdio.h>
 int main() {
-    int a, b, c;
+    int a, b, c; //variables to store the input numbers and the result 
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
     c=a+b; // Calculate the sum of a and b
     printf("Sum = %d\n", c); // Output the result hii hello 
-    return 0;
+    return 0; 
 }
